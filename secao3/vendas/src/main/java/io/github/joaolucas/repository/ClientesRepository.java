573d42e386840784e7a49d8427204ac63afbcef1
joao-lusa <joaosombrio@rede.ulbra.br>
2022-07-25 17:01:36 -0300

    Commit initial

---
+++ secao3/vendas/src/main/java/io/github/joaolucas/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.joaolucas.repository;
+
+import io.github.joaolucas.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base a salvar o cliente
+
+    }
+}
